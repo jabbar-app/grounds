47926a5501724b6b4b2c01cd1ededf8d114ffee2
Jab <jabbarpanggabean@gmail.com>
2025-05-10 10:54:01 +0700

    feat: add api to check-mail

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/check-email', function (Request $request) {
+    $email = $request->query('email');
+
+    $exists = User::where('email', $email)->exists();
+
+    return response()->json([
+        'exists' => $exists,
+    ]);
+});
